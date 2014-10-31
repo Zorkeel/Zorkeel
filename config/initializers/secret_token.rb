4046e9caa95592397ec84305bb592f90fc04cc2a
Alex <contact@jumboweb.fr>
2014-10-31 22:19:29 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zorkeel::Application.config.secret_key_base = 'd8df66df20494828d933b6babc89f876e9e91ad20b08ce20430c64cd4e8faeb27e9a099d1110c7e2481c2560639d5b0b64969e26cb21efb48aa3b94b542ed9d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zorkeel::Application.config.session_store :cookie_store, key: '_zorkeel_session'
